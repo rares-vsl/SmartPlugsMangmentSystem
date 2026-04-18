87cb4821d2f3accb99e0ab5bc4b1f668f39d5a8a
rares vasiliu <rv.vasiliu@gmail.com>
2026-04-18 15:48:03 +0200

    feat: add application layer with repository port and service impl
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ smartplugservice/application/SmartPlugServiceImpl.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from smartplugservice.application.SmartPlugService import SmartPlugsService
+from smartplugservice.application.SmartPlugsRepository import SmartPlugsRepository
+from smartplugservice.domain.SmartPlug import SmartPlug
+
+
+class SmartPlugsServiceImpl(SmartPlugsService):
+    def __init__(self, repository: SmartPlugsRepository):
+        self.repository = repository
+
+    def get_all_plugs(self) -> List[SmartPlug]:
+        return self.repository.get_all_plugs()
+
+    def get_plug_by_id(self, plug_id: str) -> SmartPlug:
+        return self.repository.get_plug_by_id(plug_id)
+
+    def switch_plug(self, plug_id: str) -> tuple[bool, str]:
+        return self.repository.switch_plug(plug_id)
---
+++ smartplugservice/application/SmartPlugsRepository.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from smartplugservice.domain.SmartPlug import SmartPlug
+
+class SmartPlugsRepository(ABC):
+    @abstractmethod
+    def get_all_plugs(self) -> List[SmartPlug]:
+        pass
+    @abstractmethod
+    def get_plug_by_id(self, plug_id: str) -> SmartPlug:
+        pass
+    @abstractmethod
+    def switch_plug(self, plug_id: str) -> tuple[bool, str]:
+        pass
